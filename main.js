099506a01f7ddb57d8a69da0d3c88cd46c3128f4
aravindhvetri <2002aravinthv@gmail.com>
2023-02-06 15:50:25 +0530

    ubdate

---
+++ main.js
@@ -0,0 +1,9 @@
+//using reduce-method:
+
+const numbers = [1,2,3,4,5];
+  let numVal = numbers.reduce(function(value1,value2,value3){
+	  return value1 * value2; 
+	        
+  });
+  console.log(numVal/3);
+  
